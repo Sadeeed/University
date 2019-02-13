79e9a2118caa124e140138caaa453566c69edf58
Sadeed <2444774+Sadeeed@users.noreply.github.com>
2019-02-14 01:10:06 +0500

    added algorithm , removed usless code. Still needs work

--- Decimal_to_Binary.c
+++ Decimal_to_Binary.c
@@ -1,26 +1,20 @@
+/*
+A Program to convert decimal to binary
+
+Algorithm to convert Decimal to Binary number
+
+    Divide the input decimal number by 2 and store the remainder.
+    Store the quotient back to the input number variable.
+    Repeat this process till quotient becomes zero.
+    Equivalent binary number will be the remainders in above process in reverse order.
+*/
 #include <stdio.h>
 #include <math.h>
 int main(void)
 {
-    int decimal = 0 , binary = 0 ;
-    int i , lcm; 
+    int decimal = 0 , binary = 0 , i = 0 ,x=2;
     printf ("Enter a decimal number\n");
     scanf("%d" , &decimal);
-    /*for(lcm = 0; lcm > 1; i++)
-    {
-        lcm = decimal % 2;
-        printf("%d\n",lcm);
-    }
-    */
-    lcm = decimal % 2;
-    printf("%d\n",lcm);
-    lcm = decimal / 2;
-    printf("%d\n",lcm);   
-    printf("%d\n",decimal);
-
-
-
-
-
+    
     return 0;
 }
