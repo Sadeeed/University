c6c0e747ffbee0fb441398c1d0fbeb43a87dc122
Sadeeed <sadeeedw@gmail.com>
2022-05-18 14:02:13 +0500

    📦 add mongodb

--- CSC336 - WEB/Javascript/node-express-basics/index.js
+++ CSC336 - WEB/Javascript/node-express-basics/index.js
@@ -1,9 +1,16 @@
-const express = require('express')
+import express from "express"
+import mongoose from "mongoose"
+
 const app = express()
 const port = 3000
 
+// const uri = 'mongodb+srv://icesoup:pegasis8@aloo.e7een.mongodb.net/?retryWrites=true&w=majority'
+const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000'
+
+mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World!`)
 })
 
 app.listen(port, () => {
