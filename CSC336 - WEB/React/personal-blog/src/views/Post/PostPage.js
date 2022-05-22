63810354ba76fd5c15952a01541cc3eff890a30a
Sadeeed <sadeeedw@gmail.com>
2022-05-22 16:59:08 +0500

    :zap: send props

--- CSC336 - WEB/React/personal-blog/src/views/Post/PostPage.js
+++ CSC336 - WEB/React/personal-blog/src/views/Post/PostPage.js
@@ -9,7 +9,12 @@ const PostPage = (props) => {
       <div className="container main-container flex on-phone--column extended">
         <SidebarLeft />
         <main className="main full-width">
-          <MainArticle />
+          <MainArticle
+            title="Test"
+            subtitle="Sample Article"
+            published="May 22, 2022"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum ligula et lacinia vestibulum. Quisque nec libero elit. Curabitur rhoncus finibus ipsum, ac pharetra nunc fringilla ac. Etiam eget enim et arcu ornare scelerisque ut quis dolor. Mauris vitae euismod ipsum, quis dictum tortor. Integer accumsan, metus et consectetur scelerisque, leo odio malesuada lorem, a tempor lectus leo eu enim. Nullam consectetur cursus turpis, id placerat dolor vulputate in. "
+          />
           <Footer />
         </main>
         {/* <SidebarRightPost/> */}
