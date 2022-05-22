c556b79535133e9652ae8c0df9e8f5433ccb997a
Sadeeed <sadeeedw@gmail.com>
2022-05-22 14:12:54 +0500

    :bug: fix about page ui

--- CSC336 - WEB/React/personal-blog/src/views/About/AboutPage.js
+++ CSC336 - WEB/React/personal-blog/src/views/About/AboutPage.js
@@ -3,15 +3,17 @@ import Footer from "../../components/Footer/Footer";
 import SidebarLeft from "../../components/Sidebar/SidebarLeft";
 
 const AboutPage = (props) => {
-    return (
-        <div className="container main-container flex on-phone--column extended">
-            <SidebarLeft/>
-            <main className="main full-width">
-                <MainArticle/>
-                <Footer />
-            </main>
-        </div>
-    );
-}
- 
+  return (
+    <div className="article-page">
+      <div className="container main-container flex on-phone--column extended">
+        <SidebarLeft />
+        <main className="main full-width">
+          <MainArticle />
+          <Footer />
+        </main>
+      </div>
+    </div>
+  );
+};
+
 export default AboutPage;
